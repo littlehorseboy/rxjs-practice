b05ad3eb7b14ebd01cc34b42d1d7b459506f3ddd
horse <littlehorse_boy@gmail.com>
2019-02-02 18:30:33 +0800

    一般 JS 綁定事件

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
-/* global rxjs */
+const handler = (e) => {
+  console.log(e);
+  document.body.removeEventListener('click', handler);
+};
 
-const observable = rxjs.Observable.create((observer) => {
-  try {
-    observer.next('Jerry');
-    observer.next('Anna');
-    // throw new Error('some exception');
-    observer.complete();
-    observer.next('Unreachable code');
-  } catch (e) {
-    observer.error(e);
-  }
-  setTimeout(() => {
-    observer.next('RxJS');
-  }, 30);
-});
-
-console.log('start');
-
-observable.subscribe(
-  (value) => { console.log(value); },
-  (error) => { console.error(error); },
-  () => { console.log('complete'); },
-);
-
-console.log('end');
+document.body.addEventListener('click', handler);
