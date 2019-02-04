ef77980ebff1c4157ad2ffc56a9adb7ff94a552b
horse <littlehorse_boy@gmail.com>
2019-02-05 01:25:24 +0800

    feat: jss scroll drag

--- src/index.js
+++ src/index.js
@@ -1,28 +1,91 @@
-/* global Rx */
+/* global Rx, jss, jssPresetDefault */
 
-const dragDOM = document.createElement('div');
-dragDOM.style.width = '100px';
-dragDOM.style.height = '100px';
-dragDOM.style.backgroundColor = 'red';
-dragDOM.style.position = 'absolute';
-dragDOM.style.cursor = 'all-scroll';
+jss.default.setup(jssPresetDefault.default());
 
-document.body.appendChild(dragDOM);
+const styles = {
+  body: {
+    height: '300vh',
+  },
+  anchorDiv: {
+    width: '100%',
+    height: '360px',
+  },
+  videoDiv: {
+    '&.videoDiv-fixed': {
+      position: 'fixed',
+      top: '10px',
+      left: '10px',
+      width: '320px',
+      height: '180px',
+      cursor: 'all-scroll',
+      '&:hover': {
+        '& $masker': {
+          display: 'block',
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          zIndex: 1,
+        },
+      },
+    },
+  },
+  masker: {
+    display: 'none',
+  },
+  video: {
+    width: '100%',
+    'max-width': '800px',
+  },
+};
 
-const validValue = (value, max, min) => Math.min(Math.max(value, min), max);
+const { classes } = jss.default.createStyleSheet(styles).attach();
 
-const mouseDown = Rx.Observable.fromEvent(dragDOM, 'mousedown');
-const mouseUp = Rx.Observable.fromEvent(document.body, 'mouseup');
-const mouseMove = Rx.Observable.fromEvent(document.body, 'mousemove');
+document.body.classList.add(classes.body);
+const anchorDiv = document.createElement('div');
+anchorDiv.classList.add(classes.anchorDiv);
+const videoDiv = document.createElement('div');
+videoDiv.classList.add(classes.videoDiv);
+const masker = document.createElement('div');
+masker.classList.add(classes.masker);
+const video = document.createElement('video');
+video.controls = true;
+video.classList.add(classes.video);
+const source = document.createElement('source');
+source.type = 'video/ogg';
+source.src = 'http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_stereo.ogg';
 
-mouseDown
-  .map(() => mouseMove.takeUntil(mouseUp))
-  .concatAll()
-  .withLatestFrom(mouseDown, (move, down) => ({
-    x: validValue(move.clientX - down.offsetX, window.innerWidth - dragDOM.clientWidth, 0),
-    y: validValue(move.clientY - down.offsetY, window.innerHeight - dragDOM.clientHeight, 0),
-  }))
-  .subscribe((pos) => {
-    dragDOM.style.left = `${pos.x}px`;
-    dragDOM.style.top = `${pos.y}px`;
+video.appendChild(source);
+videoDiv.appendChild(masker);
+videoDiv.appendChild(video);
+anchorDiv.appendChild(videoDiv);
+document.querySelector('#app').appendChild(anchorDiv);
+
+const scroll = Rx.Observable.fromEvent(document, 'scroll');
+scroll
+  .map(() => anchorDiv.getBoundingClientRect().bottom < 0)
+  .subscribe((bool) => {
+    if (bool) {
+      videoDiv.classList.add('videoDiv-fixed');
+    } else {
+      videoDiv.classList.remove('videoDiv-fixed');
+    }
   });
+
+// const validValue = (value, max, min) => Math.min(Math.max(value, min), max);
+
+// const mouseDown = Rx.Observable.fromEvent(videoDiv, 'mousedown');
+// const mouseUp = Rx.Observable.fromEvent(document.body, 'mouseup');
+// const mouseMove = Rx.Observable.fromEvent(document.body, 'mousemove');
+
+// mouseDown
+//   .map(() => mouseMove.takeUntil(mouseUp))
+//   .concatAll()
+//   .withLatestFrom(mouseDown, (move, down) => ({
+//     x: validValue(move.clientX - down.offsetX, window.innerWidth - videoDiv.clientWidth, 0),
+//     y: validValue(move.clientY - down.offsetY, window.innerHeight - videoDiv.clientHeight, 0),
+//   }))
+//   .subscribe((pos) => {
+//     videoDiv.style.left = `${pos.x}px`;
+//     videoDiv.style.top = `${pos.y}px`;
+//   });
